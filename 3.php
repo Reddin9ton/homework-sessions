8a86b2942d5820ec14031b7fc5e1e5d8f485ea62
red <artikus256@gmail.com>
2024-04-07 15:31:47 +0300

    bag fix

--- 3.php
+++ 3.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
-$_SESSION['counter'] += 1;
+$_SESSION['counter'] = ($_SESSION['counter'] ?? 0) + 1;
 if ($_SESSION['counter'] % 3 == 0) {
     header("Location: /4.php",TRUE,302);
+    exit;
 
 };
